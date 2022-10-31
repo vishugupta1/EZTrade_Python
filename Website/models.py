0186e9997b5140da7950d62e4bcb6ea775846864
Vishu Gupta <guptavishu55@gmail.com>
2022-10-30 23:31:54 -0700

    new

--- Website/models.py
+++ Website/models.py
@@ -1,1 +1,20 @@
+from cgi import parse_multipart
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
 
+class Data(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(150))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    userId = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+
+
+class User(db.model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    firstName = db.Column(db.String(150))
+    lastName = db.Column(db.String(150))
