8ff8325ba3bfaad7a9ce43fa821afdbbe8878f31
Vishu Gupta <guptavishu55@gmail.com>
2022-11-02 00:41:56 -0700

    Full application

--- Website/models.py
+++ Website/models.py
@@ -3,18 +3,24 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-class Data(db.Model):
+class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(150))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     userId = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+
+# class Stock(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     data = db.Column(db.String(150))
+#     date = db.Column(db.DateTime(timezone=True), default=func.now())
+#     userId = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
-
-
-class User(db.model, UserMixin):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     firstName = db.Column(db.String(150))
     lastName = db.Column(db.String(150))
+    notes = db.relationship('Note')
